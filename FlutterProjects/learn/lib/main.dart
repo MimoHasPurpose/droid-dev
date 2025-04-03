1b516e07f25b75bddf32b5d4fe9449f79a6b3adb
Sanjay Singh <77540699+MimoHasPurpose@users.noreply.github.com>
2025-04-03 17:22:16 +0530

    update

--- FlutterProjects/learn/lib/main.dart
+++ FlutterProjects/learn/lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    
+  runApp(  MaterialApp(
+    home: Scaffold(
+      appBar:AppBar(
+        title: const Text('mimo is titlle'),
+        backgroundColor: Colors.brown[700],
+        centerTitle: true,
+      ),
+      body: const Text('hello mimossssa!'),
+    )
   ));
 }
